7fe973a457949a7155bb80d0b710d59fc7ce086e
Arijeet Roy <blakelabs57@gmail.com>
2025-08-04 22:19:30 +0530

    practice

--- prac_02/prac_02/src/App.jsx
+++ prac_02/prac_02/src/App.jsx
@@ -1,9 +1,16 @@
-function App() {
+import { useState } from "react"
 
+function App() {
+const [title, setTitle] = useState("My name is Roy"); //this is the base state
+
+function updateTitle() {
+  setTitle("My name is " + Math.random()); //.this is what the state will look after clicking the button
+}
   return (
     // we can also use <div> in the below line to store the two Header siblings, but using <> </> is much better as it does not introduce a new element
     <>  
-      <Header title="ROY"></Header>
+      <button onClick={updateTitle}>Update the title</button> {/* calling the function for the button */}
+      <Header title={title}></Header> {/*by default it uses "My name is Roy" but on clicking the onClick, it changes to the desired result*/}
       <Header title="ROY"></Header>
     </>
   )
