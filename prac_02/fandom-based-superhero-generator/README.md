568be727f8b67c7a4a61944c8b2901faa19be024
Arijeet Roy <blakelabs57@gmail.com>
2025-08-15 22:18:44 +0530

    fandom based superhero generator

---
+++ prac_02/fandom-based-superhero-generator/README.md
@@ -0,0 +1,4 @@
+Here what we will do is there will be 3 routes, each route will handle a specific backend route as well and get the required data for the mood
+in each route there will be option to get the quote related to that specific mood
+user will navigate through the tabs and will get qotes as required
+will be using react-router-dom
---
+++ prac_02/fandom-based-superhero-generator/src/server/server.cjs
@@ -0,0 +1,49 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const marvel = [
+    {id: 1, title: "Hulk"},
+    {id: 2, title: "Iron Man"},
+    {id: 3, title: "Captain America"},
+    {id: 4, title: "Hawkeye"},
+    {id: 5, title: "Black Widow"},
+]
+
+const dc = [
+    {id: 1, title: "Batman"},
+    {id: 2, title: "Aquaman"},
+    {id: 3, title: "Superman"},
+    {id: 4, title: "Wonder Woman"},
+    {id: 5, title: "Flash"},
+]
+
+const theBoys = [
+    {id: 1, title: "Homelander"},
+    {id: 2, title: "Billy Butcher"},
+    {id: 3, title: "Soldier Boy"},
+    {id: 4, title: "A-Train"},
+    {id: 5, title: "Black Noir"},
+]
+
+app.get("/dc", function(req, res) {
+    const randomIndex = Math.floor(Math.random() * marvel.length);
+    res.json(marvel[randomIndex]);
+})
+
+app.get("/marvel", function(req, res) {
+    const randomIndex = Math.floor(Math.random() * marvel.length);
+    res.json(marvel[randomIndex]);
+})
+
+app.get("/theBoys", function(req, res) {
+    const randomIndex = Math.floor(Math.random() * marvel.length);
+    res.json(marvel[randomIndex]);
+})
+
+app.listen(3000, () => {
+    console.log("The server is running on port 3000");
+})
