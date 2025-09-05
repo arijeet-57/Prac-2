e921cb12dc9217b6efc560814eb79b3a65ccda2c
Arijeet Roy <blakelabs57@gmail.com>
2025-09-05 23:02:08 +0530

    useDebounce

--- Prac_03/useful-hooks/src/App.jsx
+++ Prac_03/useful-hooks/src/App.jsx
@@ -21,16 +21,37 @@ function useIsOnline() {
 }
 
 
+function useDebounce(value, timeout) {
+  const [debounceValue, setDebounceValue] = useState(value);
+
+  useEffect(() => {
+    let timeoutNumber = setTimeout(() => {
+      setDebounceValue(value);
+    }, timeout);
+
+    return () => {
+      clearTimeout(timeoutNumber);
+    }
+  }, [value])
+
+  return debounceValue;
+}
+
+
 
 function App() {
   const isOnline = useIsOnline();
-
+  const [value, setValue] = useState(0);
+  const debouncedValue = useDebounce(value, 500);
+ 
  if(isOnline) {
   return <div> I am online</div>
  }
 
 
   return <div>
+    Debounced Value is {debouncedValue}
+    <input type="text" onChange={e = setValue(e.target.value)} />
     Offline
   </div>
  }
