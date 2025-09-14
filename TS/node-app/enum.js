751021ccbbd6848a8f76f962244ba5690fd218ac
Arijeet Roy <blakelabs57@gmail.com>
2025-09-14 22:05:42 +0530

    TS practice basics

---
+++ TS/node-app/enum.js
@@ -0,0 +1,16 @@
+var Direction;
+(function (Direction) {
+    Direction[Direction["Up"] = 0] = "Up";
+    Direction[Direction["Down"] = 1] = "Down";
+    Direction[Direction["Left"] = 2] = "Left";
+    Direction[Direction["Right"] = 3] = "Right";
+})(Direction || (Direction = {}));
+function doSomething(keyPressed) {
+    if (keyPressed == Direction.Up) {
+    }
+}
+doSomething(Direction.Up);
+console.log(Direction.Up);
+console.log(Direction.Down);
+console.log(Direction.Left);
+console.log(Direction.Right);
---
+++ TS/node-app/enum.ts
@@ -0,0 +1,19 @@
+enum Direction {
+    Up,
+    Down,
+    Left,
+    Right
+}
+
+function doSomething(keyPressed :Direction) {
+    if(keyPressed == Direction.Up){
+
+    }
+}
+
+doSomething(Direction.Up);
+
+console.log(Direction.Up);
+console.log(Direction.Down);
+console.log(Direction.Left);
+console.log(Direction.Right);
