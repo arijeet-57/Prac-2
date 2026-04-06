ed334300934c54878594e72622f1f812db40b2f0
arijeet-57 <blakelabs57@gmail.com>
2026-04-06 18:27:20 +0000

    prac: arrays adt

--- dsa/array.cpp
+++ dsa/array.cpp
@@ -2,17 +2,50 @@
 
 using namespace std;
 
-int main () {
-    int x = 5;
-    int *p = &x;
+class Array //defining a class for array that would provide a blue print to work on
+{
+private:  //variables can only be accessed inside the class
+    int *A;
+    int size;
+    int length;
 
-    cout<<"Value of x: " << x <<endl;
-    cout<<"Address of x: " << &x << endl;
-    cout <<"Value of p: " << p << endl;
-    cout << "Value using pointer (*p): " << *p << endl;
+public: //can be accessed from outside the class as well
+    Array(int size) {       //initialising tghe instance for defininf the number of max elements to be stored in an array
+        this->size = size;
+        A = new int[size];
+    }
+
+    void create() {   //create fuction to create the array
+        cout<<"Enter the number of elements for the array: "<<endl;
+        cin>>length;
+       
+        cout<<"Enter the elements of the array:"<<endl;
+        for(int i=0;i<length;i++)
+            cin>>A[i];
+    }
+
+    void display() { //display ing the array using this function
+        for (int i=0;i<length;i++)
+            cout<<A[i]<<" ";
+    }
+
+    ~Array() { //this is a destructor , this runs automatically when an object is destroyed
+        delete[] A;
+        cout<<"Destroyed Array"<< endl;
+    }
+
+
+};
+
+int main () {
+
+    Array arr(10); //defining the max number of elements that can be input in this array, arr is an object of the class Array
+
+    arr.create(); //calling the create function using an object
+    arr.display(); //calling the display function using the same arr object
+
 
     return 0;
-
 }
 
 
